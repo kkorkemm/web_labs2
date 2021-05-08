3bcc6a9915252e3a0d45042c2d8d221b4288dd49
korkem <73012254+kkorkemm@users.noreply.github.com>
2021-05-08 23:27:56 +0600

    лаб28

--- inc/log.inc.php
+++ inc/log.inc.php
@@ -1,1 +1,12 @@
-<?
+<?php
+    $dt = date("d-m-Y H:i:s");
+    $page = $_SERVER['REQUEST_URI'];
+    $ref = $_SERVER['HTTP_REFERER'];
+    $path = "$dt  |  $page  |  $ref \n";
+
+    if (!is_dir("log")) {
+        mkdir("log");
+    }
+    $file = fopen("log/".PATH_LOG.".txt", 'a') or die("Не удалось создать файл");
+    fwrite($file, $path);
+    fclose($file);
--- inc/view-log.inc.php
+++ inc/view-log.inc.php
@@ -1,1 +1,16 @@
-<?
+<?php
+
+    if (is_file('log/'.PATH_LOG.'.txt')) {
+        $file = file('log/'.PATH_LOG.'.txt'); 
+        
+        echo "<ol>";
+        foreach($file as $row) {
+            echo "<li>";
+            
+            list($date, $ref, $page) = explode("|", $row);
+            echo "<b>$date</b> \t $ref --> $page";
+
+            echo "</li>";
+        }
+        echo "</ol>";
+    }
